4ddab3a35bc55ba848b43a19113bfbc4c488799d
ivaquero <msaintms@outlook.com>
2026-04-15 00:55:04 +0800

    feat: add help subcommand

--- main.go
+++ main.go
@@ -123,6 +123,13 @@ func main() {
 			}
 		}
 
+	case "help":
+		if len(os.Args) > 2 {
+			showCommandHelp(os.Args[2])
+		} else {
+			showHelp()
+		}
+
 	default:
 		fmt.Printf("Unknown command: %s\n", command)
 		showHelp()
@@ -139,17 +146,92 @@ func showHelp() {
 	fmt.Println("  show [id <id>]                Show account details")
 	fmt.Println("  open <number> [id <id>]       Open specific email in browser")
 	fmt.Println("  export <path> [id <id>]       Export account data to specified path")
+	fmt.Println("  help                          Show this help message")
 	fmt.Println("\nOptions:")
 	fmt.Println("  id <id>                       Specify account ID for operations")
-	fmt.Println("\nExamples:")
-	fmt.Println("  gotmail new")
-	fmt.Println("  gotmail list")
-	fmt.Println("  gotmail msg")
-	fmt.Println("  gotmail msg id abc123")
-	fmt.Println("  gotmail show")
-	fmt.Println("  gotmail show id abc123")
-	fmt.Println("  gotmail open 1")
-	fmt.Println("  gotmail open 1 id abc123")
-	fmt.Println("  gotmail export /path/to/backup")
-	fmt.Println("  gotmail export /path/to/backup id abc123")
+}
+
+func showCommandHelp(command string) {
+	switch command {
+	case "new":
+		fmt.Println("Create a new account")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail new")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Creates a new temporary email account with a random address")
+		fmt.Println("  The account credentials will be stored locally for future use")
+
+	case "list":
+		fmt.Println("List all accounts")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail list")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Displays all stored email accounts with their IDs and addresses")
+
+	case "msg":
+		fmt.Println("Fetch and list messages")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail msg [id <account_id>]")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Retrieves messages from the specified account or the default account")
+		fmt.Println("\nExamples:")
+		fmt.Println("  gotmail msg                    # Fetch from default account")
+		fmt.Println("  gotmail msg id abc123          # Fetch from specific account")
+
+	case "del":
+		fmt.Println("Delete account")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail del [id <account_id>]")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Removes the specified account or the default account from storage")
+		fmt.Println("\nExamples:")
+		fmt.Println("  gotmail del                    # Delete default account")
+		fmt.Println("  gotmail del id abc123          # Delete specific account")
+
+	case "show":
+		fmt.Println("Show account details")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail show [id <account_id>]")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Displays detailed information about the specified account")
+		fmt.Println("\nExamples:")
+		fmt.Println("  gotmail show                   # Show default account")
+		fmt.Println("  gotmail show id abc123         # Show specific account")
+
+	case "open":
+		fmt.Println("Open specific email in browser")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail open <number> [id <account_id>]")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Opens the specified email message in your default web browser")
+		fmt.Println("\nExamples:")
+		fmt.Println("  gotmail open 1                 # Open first message from default account")
+		fmt.Println("  gotmail open 3 id abc123       # Open third message from specific account")
+
+	case "export":
+		fmt.Println("Export account data to specified path")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail export <path> [id <account_id>]")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Exports account data to the specified file path")
+		fmt.Println("\nExamples:")
+		fmt.Println("  gotmail export /tmp/backup.json          # Export default account")
+		fmt.Println("  gotmail export /tmp/backup.json id abc123 # Export specific account")
+
+	case "help":
+		fmt.Println("Show help information")
+		fmt.Println("\nUsage:")
+		fmt.Println("  gotmail help [command]")
+		fmt.Println("\nDescription:")
+		fmt.Println("  Shows general help or detailed help for a specific command")
+		fmt.Println("\nExamples:")
+		fmt.Println("  gotmail help                   # Show general help")
+		fmt.Println("  gotmail help msg               # Show help for msg command")
+
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		fmt.Println("\nAvailable commands:")
+		fmt.Println("  new, list, msg, del, show, open, export, help")
+		fmt.Println("\nUse 'gotmail help <command>' for detailed help on a specific command")
+	}
 }
